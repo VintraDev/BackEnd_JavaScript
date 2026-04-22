878a72a2e9a8a217f43ddf0a7d944f651427691d
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-22 09:31:47 -0300

    Feat - implementação do primeiro proojeto desenvolvido

--- Node_Rocketseat/server.js
+++ Node_Rocketseat/server.js
@@ -13,21 +13,71 @@
 // DELETE localhost:3000/videos/1
 
 import { fastify } from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js';
 
 const server = fastify();
 
-server.get('/', () => {
-    return 'Hello World'
+// const database = new DatabaseMemory()
+
+const database = new DatabasePostgres
+
+// GET, POST, PUT, DELETE
+
+// POST http://localhost:3000/videos
+// PUT http://localhost:3000/videos/3
+
+// Request Body
+
+server.post('/videos', async (request, response) => {
+
+    const { title, description, duration} = request.body
+
+     await database.create({
+        title,
+        description,
+        duration,
+    });
+
+    return response.status(201).send('Vídeo publicado com sucesso!');
 });
 
-server.get('/hello', () => {
-    return 'Hello Vinicius'
+server.get('/videos', async (request, response) => {
+    const search = await request.query.search
+
+    const video = await database.list(search);
+
+    console.log(video);
+
+    return video;
 });
 
-server.get('/node', () => {
-    return 'Hello Node'
-});
+// Route Parameter
+
+server.put('/videos/:id', async (request, response) => {
+    const videoId = request.params.id;
+    const { title, description, duration} = request.body;
+
+    const video = await database.update(videoId, {
+        title,
+        description,
+        title,
+    });
+
+    return response.status(204).send('Vídeo editado com sucesso!')
+})
+
+server.delete('/videos/:id', async (request, response) => {
+    const videoId = request.params.id;
+
+    await database.delete(videoId);
+
+    return response.status(204).send('Vídeo deletado com sucesso!');
+})
 
 server.listen({
     port: 3000
 });
+
+// CRUD (Create Read Update Delete)
+
